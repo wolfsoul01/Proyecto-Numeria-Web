0b2656c28afa5008c5567eee5c9d4e0c2656cbdd
guilledev <guillermoprogcc@gmail.com>
2023-04-17 00:44:39 -0400

    Organizando y creando carpeta Backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-import Calcular from "./components/Calcular";
+import Calcular from "./pages/Calcular";
 const App = () => {
   return (
     <>
