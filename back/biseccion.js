2aa09bc067c32650eadbb46e3a7eccb81ced15df
wolfsoul <alejandrof011021@gmail.com>
2023-04-18 13:08:08 -0400

    Se creo el metodo de biseccion listo para el el front(aun falta probarlo) Se agregaron comentarios de guia en back

---
+++ back/biseccion.js
@@ -0,0 +1,46 @@
+const mBiseccion = (p, a, b, err) => {
+    let c;
+    let iteracion = 0;
+    let resultados = [];
+  
+    // Iteramos hasta alcanzar el error
+    while (math.abs(b - a) > err) {
+      //  El punto medio del intervalo
+      c = (a + b) / 2;
+      iteracion++;
+  
+      // Evaluamos la función en el punto medio
+      const fc = f(c);
+  
+      // Guardamos los resultados de cada iteración en un objeto
+      let resultado = {
+        iteracion: iteracion,
+        a: a,
+        b: b,
+        c: c,
+        fc: fc
+      };
+      resultados.push(resultado);
+  
+      // Si la función evaluada en el punto medio es cero, hemos encontrado la raíz
+      if (fc === 0) {
+        break;
+      }
+  
+      // entonces la raíz está en el intervalo [c, b]
+      if (math.sign(fc) === math.sign(f(a))) {
+        a = c;
+      }
+  
+      // entonces la raíz está en el intervalo [a, c]
+      else {
+        b = c;
+      }
+    }
+  
+    // Devolve un objeto con  resultado final y  los resultados de las iteraciones
+    return {
+      raiz: c,
+      iteraciones: resultados
+    };
+  };
--- back/evaluciacion.js
+++ back/evaluciacion.js
@@ -1,14 +1,19 @@
-import * as math from 'mathjs';
+import * as math from "mathjs";
 
-export const evaluarFuncion = (p, px=1, py=1) => {
+// Crear y evaluar la funcion a la vez 
+export const evaluarFuncion = (p, px = 1, py = 1) => {
   const f = math.parse(p).compile();
-  const scope={x:px}
-   return f.evaluate(scope);
+  const scope = { x: px }; // es alcanze de kas variables que controla 
+  return f.evaluate(scope);
+};
+// para crear un funcion sola 
+export const crearFuncion = (p) => {
+  return math.parse(p).compile();
 };
 
-export const crearFuncion = (p) => {
-    return  math.parse(p).compile();
-  };
-  
 
+/*  const expr = 'a^2 + b^2';
+const scope = { a: 3, b: 4 };
+const result = math.evaluate(expr, scope);
 
+ */
