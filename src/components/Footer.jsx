1bd18c175b2b59d096cb7ab658771628f236ff90
guilledev <guillermoprogcc@gmail.com>
2023-04-20 22:14:52 -0400

    Mejorando el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-gray-800 py-4">
       <div className="container mx-auto text-center text-white">
-        © 2023 desarrollado por Alegandro , Guillermo y Dayanis. Todos los derechos reservados.
+        © 2023 creado por Alegandro , Guillermo y Dayanis. Todos los derechos reservados.
       </div>
     </footer>
   );
