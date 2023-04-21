701d83a8ccad5c2aa38608e7de608a4808d24680
wolfsoul <alejandrof011021@gmail.com>
2023-04-21 13:02:55 -0400

    Se agrego la documentacion de metodo de evaluar funcion

--- src/pages/Documentation.jsx
+++ src/pages/Documentation.jsx
@@ -18,18 +18,14 @@ function Documentation() {
       </div>
       <div className="container mx-auto my-8 px-2">
         <section id="funcion">
-          <h2 className="text-2xl font-bold mb-4">Cálculo de un polinomio por el método de Horner</h2>
+          <h2 className="text-2xl font-bold mb-4">1.Evaluación de expresiones matemáticas</h2>
           <p className="mb-4">
-            El método de Horner es una técnica para evaluar un polinomio en un punto dado de manera eficiente.
-            Para utilizar este método, se requiere una expresión polinómica expresada en forma de Horner, que
-            consiste en dividir sucesivamente los coeficientes del polinomio por el valor que se está evaluando,
-            y luego sumar el resultado a partir del término de menor grado. De esta forma, se evita tener que
-            realizar múltiples operaciones para evaluar el polinomio completo.
+          La evaluación de expresiones matemáticas es un método utilizado en matemáticas para calcular el valor de una expresión matemática. Este método se basa en el uso de reglas algebraicas y operaciones matemáticas para simplificar y evaluar expresiones matemáticas complejas.
+          La evaluación de expresiones matemáticas se utiliza comúnmente en áreas de las matemáticas como el cálculo y el análisis matemático, así como en otras áreas de la ciencia y la ingeniería donde se requiere el cálculo de valores numéricos a partir de expresiones matemáticas complejas.
+
           </p>
           <p className="mb-4">
-            El método de Horner es especialmente útil para el cálculo de polinomios de grado alto, ya que reduce
-            la cantidad de operaciones necesarias para evaluarlos. En resumen, el método de Horner es una técnica
-            eficiente y efectiva para evaluar polinomios en un punto dado.
+          Un ejemplo práctico de la evaluación de expresiones matemáticas es en la resolución de ecuaciones y sistemas de ecuaciones. Al utilizar la evaluación de expresiones matemáticas, es posible calcular los valores de las variables en una ecuación o sistema de ecuaciones, lo que permite resolver problemas complejos en áreas como la física, la ingeniería y la economía.
           </p>
         </section>
         <section id="biseccion" className="mb-20">
