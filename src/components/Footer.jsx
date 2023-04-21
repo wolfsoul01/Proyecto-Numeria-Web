44105b05cf02d317c285f565120ffc166d41e1b1
wolfsoul <alejandrof011021@gmail.com>
2023-04-21 13:27:07 -0400

    Se creo el footer con la licencia del proyecto y datos sobre este

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 py-4">
+    <footer className="bg-gray-800 py-4 ">
       <div className="container mx-auto text-center text-white">
-        © 2023 creado por Alegandro , Guillermo y Dayanis. Todos los derechos reservados.
+      "© 2023 Creado por 3 estudiantes. Este proyecto es open source y su código fuente está disponible en nuestro repositorio en GitHub con nuestros contactos.El código fuente de este proyecto está disponible bajo la licencia Apache,¡estamos abiertos a recibir tus aportes! ¡Gracias por tu interés y tu apoyo!"
       </div>
     </footer>
   );
