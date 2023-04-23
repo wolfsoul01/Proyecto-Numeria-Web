b13593efd12da4d9e4c43b0c1350e854691d3f2c
guilledev <guillermoprogcc@gmail.com>
2023-04-23 19:18:39 -0400

    Arregle el Responsive de la tabla de Biseccion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import Guille from '../assets/Guille.jpg'
-import Ale from '../assets/Ale.jpg'
-import Dayanis from '../assets/Dayanis.jpg'
+import React from "react";
+import Guille from "../assets/Guille.jpg";
+import Ale from "../assets/Ale.jpg";
+import Dayanis from "../assets/Dayanis.jpg";
 function Footer() {
   return (
-    <footer className="bg-gray-800 py-4 text-11px">
-      <div className="container mx-auto text-center text-white">
-        <div className="flex flex-col md:flex-row justify-center items-center">
-          <div className="mb-6 md:mb-0 md:mr-12">
-            <div className="flex items-center">
-              <img className="w-8 h-8 rounded-full" src={Guille} />
-              <p className="ml-2">Guillermo</p>
+    <footer className='bg-gray-800 py-4 text-11px'>
+      <div className='container mx-auto text-center text-white'>
+        <div className='flex flex-col md:flex-row justify-center items-center'>
+          <div className='mb-6 md:mb-0 md:mr-12'>
+            <div className='flex items-center mt-2'>
+              <img
+                src={Dayanis}
+                alt='Dayanis'
+                className='w-8 h-8 rounded-full'
+              />
+              <p className='ml-2'>Dayanis</p>
             </div>
-            <div className="flex items-center mt-2">
-              <img src={Ale} alt="Alejandro" className="w-8 h-8 rounded-full" />
-              <p className="ml-2">Alejandro</p>
+            <div className='flex items-center mt-2'>
+              <img className='w-8 h-8 rounded-full' src={Guille} />
+              <p className='ml-2'>Guillermo</p>
             </div>
-            <div className="flex items-center mt-2">
-              <img src={Dayanis} alt="Dayanis" className="w-8 h-8 rounded-full" />
-              <p className="ml-2">Dayanis</p>  
-            </div>  
+            <div className='flex items-center mt-2'>
+              <img src={Ale} alt='Alejandro' className='w-8 h-8 rounded-full' />
+              <p className='ml-2'>Alejandro</p>
+            </div>
           </div>
 
-          <div className="mb-6 md:mb-0">
-            <p>© 2023 Creado bajo licencia Apache 2.0. Código abierto disponible en GitHub.</p> 
+          <div className='mb-6 md:mb-0'>
+            <p>
+              © 2023 Creado bajo licencia Apache 2.0. Código abierto disponible
+              en GitHub.
+            </p>
           </div>
 
           <div>
--- src/components/TableBiseccion.jsx
+++ src/components/TableBiseccion.jsx
@@ -1,51 +1,56 @@
 import React from "react";
 
-const TableBiseccion = ({ resultsArray , polinomioUsado }) => {
+const TableBiseccion = ({ resultsArray, polinomioUsado }) => {
   return (
     <>
-      <h1 className='text-center text-xl text-orange-500 font-extrabold m-0'>Resultados</h1>
-      <table className='bg-white table-auto mt-8 border-collapse border border-gray-300'>
-        <thead>
-          <tr>
-            <th className='border border-gray-300 px-4 py-2'>{"Iteración"}</th>
-            <th className='border border-gray-300 px-4 py-2'>{"a"}</th>
-            <th className='border border-gray-300 px-4 py-2'>{"f(a)"}</th>
-            <th className='border border-gray-300 px-4 py-2'>{"b"}</th>
-            <th className='border border-gray-300 px-4 py-2'>{"f(b)"}</th>
-            <th className='border border-gray-300 px-4 py-2'>{"c"}</th>
-            <th className='border border-gray-300 px-4 py-2'>{"error"}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {resultsArray.resultados.map((result, index) => (
-            <tr key={index} className={index % 2 === 0 ? "bg-gray-200" : ""}>
-              <td className='border border-gray-300 px-4 py-2'>
-                {result.iteracion}
-              </td>
-              <td className='border border-gray-300 px-4 py-2'>
-                {result.a}
-              </td>
-              <td className='border border-gray-300 px-4 py-2'>
-                {result.fa}
-              </td>
-              <td className='border border-gray-300 px-4 py-2'>
-                {result.b}
-              </td>
-              <td className='border border-gray-300 px-4 py-2'>
-                {result.fb}
-              </td>
-              <td className='border border-gray-300 px-4 py-2'>
-                {result.c}
-              </td>
-              <td className='border border-gray-300 px-4 py-2'>
-                {result.fc}
-              </td>
+      <h1 className='text-center text-xl text-orange-500 font-extrabold m-0'>
+        Resultados
+      </h1>
+      <div className='overflow-x-auto max-w-full'>
+        <table className='bg-white table-auto mt-8 border-collapse border border-gray-300'>
+          <thead>
+            <tr>
+              <th className='border border-gray-300 px-4 py-2'>
+                {"Iteración"}
+              </th>
+              <th className='border border-gray-300 px-4 py-2'>{"a"}</th>
+              <th className='border border-gray-300 px-4 py-2'>{"f(a)"}</th>
+              <th className='border border-gray-300 px-4 py-2'>{"b"}</th>
+              <th className='border border-gray-300 px-4 py-2'>{"f(b)"}</th>
+              <th className='border border-gray-300 px-4 py-2'>{"c"}</th>
+              <th className='border border-gray-300 px-4 py-2'>{"error"}</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <h3 className="text-orange-500 font-black">Polinomio : <span className="text-gray-900">{polinomioUsado}</span></h3>
-      <h3 className="text-orange-500 font-black">Resultado final: <span className="text-gray-900">{resultsArray.raiz}</span></h3>
+          </thead>
+          <tbody>
+            {resultsArray.resultados.map((result, index) => (
+              <tr key={index} className={index % 2 === 0 ? "bg-gray-200" : ""}>
+                <td className='border border-gray-300 px-4 py-2'>
+                  {result.iteracion}
+                </td>
+                <td className='border border-gray-300 px-4 py-2'>{result.a}</td>
+                <td className='border border-gray-300 px-4 py-2'>
+                  {result.fa}
+                </td>
+                <td className='border border-gray-300 px-4 py-2'>{result.b}</td>
+                <td className='border border-gray-300 px-4 py-2'>
+                  {result.fb}
+                </td>
+                <td className='border border-gray-300 px-4 py-2'>{result.c}</td>
+                <td className='border border-gray-300 px-4 py-2'>
+                  {result.fc}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <h3 className='text-orange-500 font-black'>
+        Polinomio : <span className='text-gray-900'>{polinomioUsado}</span>
+      </h3>
+      <h3 className='text-orange-500 font-black'>
+        Resultado final:{" "}
+        <span className='text-gray-900'>{resultsArray.raiz}</span>
+      </h3>
     </>
   );
 };
