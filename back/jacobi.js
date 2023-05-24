9c105f1d7d669f8a4b8e90e5cc4330cd514f8b35
wolfsoul <alejandrof011021@gmail.com>
2023-05-24 14:38:49 -0400

    Se creo el metodo de jacobi listo para el front

---
+++ back/jacobi.js
@@ -0,0 +1,56 @@
+const math = require('mathjs');
+
+function jacobiMethod(matrix, b, tolerance, maxIterations) {
+  const n = matrix.length;
+  let x = math.zeros(n)._data; // Convertimos el vector a un array
+  let xNext = math.zeros(n)._data; // Convertimos el vector a un array
+  let iteration = 0;
+
+  const result = {
+    iterations: [],
+    solution: null,
+    error: null,
+    errorMessage: null
+  };
+
+  while (iteration < maxIterations) {
+    for (let i = 0; i < n; i++) {
+      let sum = 0;
+      for (let j = 0; j < n; j++) {
+        if (j !== i) {
+          sum += matrix[i][j] * x[j];
+        }
+      }
+      xNext[i] = (b[i] - sum) / matrix[i][i];
+    }
+
+    let error = math.norm(math.subtract(xNext, x), 'inf');
+
+    result.iterations.push({
+      iteration: iteration,
+      x: [...xNext],
+      error: error
+    });
+
+    if (error < tolerance) {
+      result.solution = [...xNext];
+      result.error = error;
+      return result;
+    }
+
+    x = [...xNext];
+    iteration++;
+  }
+
+  result.errorMessage = `El método de Jacobi no convergió después de ${maxIterations} iteraciones.`;
+  return result;
+}
+
+// Ejemplo de uso:
+/* const matrix = [[5, -1], [1, -3]];
+const b = [4, 1];
+const tolerance = 0.0001;
+const maxIterations = 100;
+
+const result = jacobiMethod(matrix, b, tolerance, maxIterations);
+console.log("Resultado: ", result); */
